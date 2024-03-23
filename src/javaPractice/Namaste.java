45410409197e11bc0585a3ff0e5a5744f303fb78
juropani786 <163758390+juropani786@users.noreply.github.com>
2024-03-23 11:02:54 -0400

    Hi!This is our new class

---
+++ src/javaPractice/Namaste.java
@@ -0,0 +1,15 @@
+package javaPractice;
+
+public class Namaste {
+	int n = 33;
+	String Str = "Namaskar";
+	public static void main(String[]args) {
+		System.out.println(" good evening everyone");
+
+	}
+
+	
+
+	}
+
+
